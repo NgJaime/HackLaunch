f8d2c38ab52abdf9bea85d9c10ec52cfefc02b40
Craig Bennett <craigbennett@hotmail.com>
2015-03-09 11:33:01 +0800

    Added logging

--- HackLaunch/settings/base.py
+++ HackLaunch/settings/base.py
@@ -131,6 +131,47 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'hacklaunch.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'base': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+        'hacklaunch': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+        'users': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 #############################
 # Mongoengine
 #############################
