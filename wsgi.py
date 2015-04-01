cef3f288aa1a9e5aa7e2580a5b850aa83214bf7e
Ubuntu <ubuntu@ip-172-31-24-148.ap-southeast-1.compute.internal>
2015-04-01 06:11:51 +0000

    Added production settings

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "hackLaunch.settings.production"
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
