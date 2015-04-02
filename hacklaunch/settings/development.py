3ae730557bca41606478a28115dcee95ad8c402b
Craig Bennett <craigbennett@hotmail.com>
2015-04-02 20:22:56 +0800

    Added allowed host per deployment type

--- hacklaunch/settings/development.py
+++ hacklaunch/settings/development.py
@@ -1,5 +1,7 @@
 from hacklaunch.settings.base import *
 
+ALLOWED_HOSTS = []
+
 AWS_S3_SECURE_URLS = False
 AWS_QUERYSTRING_AUTH = False     
 AWS_STORAGE_BUCKET_NAME = 'hacklaunch-images-test'
--- hacklaunch/settings/staging.py
+++ hacklaunch/settings/staging.py
@@ -2,6 +2,8 @@ from hacklaunch.settings.base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = [u'ec2-52-74-130-171.ap-southeast-1.compute.amazonaws.com']
+
 AWS_S3_SECURE_URLS = False
 AWS_QUERYSTRING_AUTH = False
 AWS_STORAGE_BUCKET_NAME = 'hacklaunch-images-test'
