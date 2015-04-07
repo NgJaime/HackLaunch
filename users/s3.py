2073b67cd354bd0ea0721297b339a8da95c3091f
Craig Bennett <craig@hacklaunch.com>
2015-04-07 21:43:53 +0800

    UUID image naming and s3 deletion on image change

--- users/s3.py
+++ users/s3.py
@@ -1,10 +1,8 @@
 import os
-from PIL import Image
-from django.core.files.storage import default_storage as storage
-from django.utils.timezone import now
+import uuid
 
 def upload_image(instance, filename):
     filename_base, filename_ext = os.path.splitext(filename)
+    name = uuid.uuid1()
 
-    return 'profiles/%s%s' % (now().strftime("%Y%m%d%H%M%S"), filename_ext.lower())
-
+    return 'profiles/%s%s' % (str(name), filename_ext.lower())
