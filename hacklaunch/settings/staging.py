e38563978a7cdf29b9b69ebc84acb6447e33af9b
Craig Bennett <craig@hacklaunch.com>
2015-04-19 00:16:36 +0800

    Added https for social redirects

--- hacklaunch/settings/staging.py
+++ hacklaunch/settings/staging.py
@@ -3,9 +3,11 @@ from hacklaunch.settings.base import *
 DEBUG = False
 
 ALLOWED_HOSTS = [u'ec2-52-74-130-171.ap-southeast-1.compute.amazonaws.com',
-		 u'52.74.32.193',
-		 u'staging.hacklaunch.com']
+		         u'52.74.32.193',
+		         u'staging.hacklaunch.com']
 
 AWS_S3_SECURE_URLS = False
 AWS_QUERYSTRING_AUTH = False
 AWS_STORAGE_BUCKET_NAME = 'hacklaunch-images-test'
+
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
