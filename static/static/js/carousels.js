c5d95fc736b77f211daab9dbcf586ecb54186e47
Craig Bennett <craig@hacklaunch.com>
2015-04-25 17:59:23 +0800

    Updated carousels home and event

--- static/static/js/carousels.js
+++ static/static/js/carousels.js
@@ -15,10 +15,9 @@ $(document).ready(function () {
 
     $('.bx-horizontal').bxSlider({
         minSlides: 2,
-        maxSlides: 2,
-        slideWidth: 360,
+        maxSlides: 5,
+        slideWidth: 300,
         slideMargin: 10,
-        slideHeight: 175,
         pager: false,
         ticker: true,
         speed: 35000,
