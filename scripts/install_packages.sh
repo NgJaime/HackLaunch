152e77b9c57da5b24465e920ef6257f94931881f
Craig Bennett <craig@hacklaunch.com>
2015-04-26 11:49:26 +0800

    Added script to install additonal packages required on ubuntu

---
+++ scripts/install_packages.sh
@@ -0,0 +1,6 @@
+apt-get install git
+apt-get install nginx
+apt-get install libjpeg-dev libfreetype6-dev zlib1g-dev libpng12-dev
+apt-get install python-virtualenv
+apt-get install libpq-dev python-dev
+apt-get install supervisor
