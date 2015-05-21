9a76ac101de4786550c1916fd375a2070daa78f4
Craig Bennett <craig@hacklaunch.com>
2015-05-21 20:47:31 +0800

    removed jslint as not supported in django jenkins

--- hacklaunch/settings/ci.py
+++ hacklaunch/settings/ci.py
@@ -20,6 +20,5 @@ INSTALLED_APPS += ['django_jenkins']
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.run_jslint',
     'django_jenkins.tasks.run_csslint',
 )
