9cedd69e99858bd554136bae0fff87c64b9c4c0e
Craig Bennett <craig@hacklaunch.com>
2015-06-10 14:29:46 +0800

    added admin for projects

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from projects.models import Project, ProjectCreators, ProjectTechnologies, Technologies, Tags
 
-# Register your models here.
+# admin.site.register(Project)
+admin.site.register(ProjectCreators)
+admin.site.register(ProjectTechnologies)
+admin.site.register(Technologies)
+admin.site.register(Tags)
+
+
+class CreatorsInline(admin.TabularInline):
+    model = Project.creators.through
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [CreatorsInline,]
+    exclude = ('elements',) #exclude the field you put the inline on so you dont have double fields
+
+admin.site.register(Project, ProjectAdmin)
